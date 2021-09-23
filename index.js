b852fd1bb620d9c789842c5a94ef02df52411aac
israelisaque <israelisaque@hotmail.com>
2021-09-22 23:11:12 -0300

    Criando o model Pergunta e salvando uma pergunta no mysql

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const express = require("express");
 const app = express();
 const bodyParser = require("body-parser");
 const connection = require("./database/database");
+const Pergunta = require("./database/Pergunta");
 
 //Database
 connection
@@ -35,8 +36,12 @@ app.get("/perguntar", (req, res) => {
 app.post("/salvarpergunta", (req, res) => {
   const titulo = req.body.titulo;
   const descricao = req.body.descricao;
-
-  res.send(`Formulário recebido! <br> Titulo: ${titulo} <br> Descrição ${descricao}`);
+  Pergunta.create({
+    titulo: titulo,
+    descricao: descricao
+  }).then(() => {
+    res.redirect("/");
+  })
 });
 
 app.listen(8080, () => {
